14bcd2b9d852a063f41e10ce1dc6e7ee9133fdca
Eric Kreutzer <eric@airkrft.com>
2014-02-06 03:26:02 -0700

    Initial commit

---
+++ source/javascripts/theme.js
@@ -0,0 +1,22 @@
+/*
+ *= require zepto
+ *= require_self
+*/
+
+(function() {
+
+  $(document).on('ready', function() {
+    $('.post').each(function() {
+      var $el    = $(this),
+          $title = $el.find('.post__caption h1'),
+          title;
+
+      if ($title.get(0)) {
+        title = $title.get(0);
+        $title.remove();
+        $el.find('.post__title').text(title.innerText);
+      }
+    });
+  });
+
+})();
